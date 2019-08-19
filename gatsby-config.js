21075535d8138fa4263fbe4b909f371cdf4eea6e
Abhishek Lohani <abhishek.lohani@macrometa.co>
2019-08-19 13:09:40 +0530

    clean up

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Nandha`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Macrometa Polling App`,
+    description: `A polling app to showcase Macrometa's JAM stack capabilities`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -25,9 +25,39 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-c8db",
+      options: {
+        config: "https://try.macrometa.io",
+        auth: {
+          tenant: "demo",
+          user: "root",
+          password: "demo"
+        },
+        collection: 'poll',
+        map: {
+          coll1: { title: "text/markdown", content: "text/markdown" }
+        }
+      }
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
